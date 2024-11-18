a5e08058d4c187e4a50380f20ab0c91542dda200
JollyRocketts <jollyaryan7@gmail.com>
2024-11-18 21:19:46 +0530

    Added files and link bar
    Signed-off-by: JollyRocketts <jollyaryan7@gmail.com>

---
+++ ai-quiz-generator/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "./components/Hero";
+import QuizGenerator from "./components/QuizGenerator";
+import Collections from "./components/Collections";
+import History from "./components/History";
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      <QuizGenerator />
+    </div>
+  );
+};
+
+export default App;
